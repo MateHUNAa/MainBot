fe8d21b23fdf6897744579b16d7e2368293d41bc
MateHUN <matehunter123gmail.com>
2020-05-18 16:58:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,10 +25,10 @@ kliens.on("message", message => {
   if (message.content.toLowerCase() === prefix + 'time?') {
     const sTime = new discord.RichEmbed()
     moment.locale('hu')
-    .setTitle(moment().format('YYYY, MMMM, hh:mm:ss'))
-    .addField("Pontos idő?:", "Év: " + moment().format("YYYY") + "\n" + "Hónap: " + moment().format("MMMM") + "\n" + moment().format("hh:mm:ss") )
-    .addField("Nap vegéig még: ", moment().endOf('day').fromNow())
-    .addField("Minden egyben ??? ", moment().format('LLLL'))
+    .setTitle(moment().add(2, 'hour').format('YYYY, MMMM, hh:mm:ss'))
+    .addField("Pontos idő?:", "Év: " + moment().add(2, 'hour').format("YYYY") + "\n" + "Hónap: " + moment().format("MMMM") + "\n" + moment().format("hh:mm:ss") )
+    .addField("Nap vegéig még: ", moment().add(2, 'hour').endOf('day').fromNow())
+    .addField("Minden egyben ??? ", moment().add(2, 'hour').format('LLLL'))
     message.channel.send(sTime)
   }
 })
